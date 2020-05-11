2adccabf48e005c63dbd78b2fb068971b8754199
Vi Co Duong <vico.duong@bom.gov.au>
2020-05-11 06:40:28 +0000

    Print input arguments used, renamed h5_grids_file argument

--- transform_grids.py
+++ transform_grids.py
@@ -99,8 +99,8 @@ def handle_arguments():
         '--output_path', '-o', required=True,
         help='Output path. Folder will be created if it doesn\'t already exist')
     parser.add_argument(
-        '--h5_grids_file', '-h5', required=True,
-        help='H5 grids file to use')
+        '--grid_file', '-g', required=True,
+        help='Grid file containing z0 factor. Expected to be in H5 format, with parameter name parameters/z0_masked')
     parser.add_argument(
         '--year_start', '-ys', required=True, type=int,
         help='Start Year. 1st Jan of start year is assumed')
@@ -122,6 +122,15 @@ def handle_arguments():
 
 if __name__ == '__main__':
     args = handle_arguments()
+    print('Transform Grids run with:')
+    print('Input glob pattern:', args.input_glob_pattern)
+    print('Output path:', args.output_path)
+    print('Grid file:', args.grid_file)
+    print('Wind variable name:', args.wind_var_name)
+    print('Start year:', args.year_start)
+    print('End year:', args.year_end)
+    print('Direction:', args.direction)
+
     period = pd.date_range(
         '{:d}-01-01'.format(args.year_start),
         '{:d}-12-31'.format(args.year_end), freq='D')
@@ -129,7 +138,7 @@ if __name__ == '__main__':
     transform(
         input_glob_pattern = args.input_glob_pattern,
         out_path = args.output_path,
-        h5_grids_file = args.h5_grids_file,
+        h5_grids_file = args.grid_file,
         wind_var_name = args.wind_var_name,
         period = period,
         direction = args.direction)
